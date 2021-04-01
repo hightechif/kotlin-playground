3a8b515960464eb6ec76949910027bae9f2d578f
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2021-04-01 18:07:07 +0700

    arithmetic operators

---
+++ kt03_arithmetic/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val x1 = 10 + 3  // 13
+    val x2 = 10 - 3  // 7
+    val x3 = 10 * 3  // 30
+    val x4 = 10 / 3  // 3
+    val x5 = 10f / 3 // 3.3333333
+    val x6 = 10 % 3  // 1
+
+    println("The value of x1 is $x1")
+    println("The value of x2 is $x2")
+    println("The value of x3 is $x3")
+    println("The value of x4 is $x4")
+    println("The value of x5 is $x5")
+    println("The value of x6 is $x6")
+}
