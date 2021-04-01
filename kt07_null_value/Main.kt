f97b567b442d6ffeac6d6753054e5c993c5f1676
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2021-04-01 18:40:34 +0700

    null value

--- kt07_null_value/Main.kt
+++ kt07_null_value/Main.kt
@@ -3,14 +3,14 @@ fun main() {
     println(x)
 
     // Way 1
-    val number11 = readLine()
-    val number12 = readLine()
-    val result1 = number11!!.toInt() + number12!!.toInt() // !! could potentially make your program crash
+    val number1 = readLine()
+    val number2 = readLine()
+    val result1 = number1!!.toInt() + number2!!.toInt() // !! could potentially make your program crash
     println(result1)
 
     // Way 2. Safer Way
-    val number21 = readLine() ?: "0"
-    val number22 = readLine() ?: "0"
-    val result2 = number21!!.toInt() + number22!!.toInt() // !! could potentially make your program crash
+    val number3 = readLine() ?: "0"
+    val number4 = readLine() ?: "0"
+    val result2 = number3.toInt() + number4.toInt()
     println(result2)
 }
